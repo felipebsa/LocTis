02b6c3c2397037dcc537f4df529587f39a557fd5
felipessantosbsa-cypher <felipessantosbsa@gmail.com>
2026-07-22 17:41:48 -0300

    feat: add client model + uniquecontraint for cpf

--- backend/models/client.py
+++ backend/models/client.py
@@ -1,8 +1,19 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from database import Base
 from datetime import datetime
-from sqlalchemy import func
+from sqlalchemy import func, ForeignKey, String, UniqueConstraint
 
 class Client(Base):
     __tablename__ = "clients"
+
     id: Mapped[int] = mapped_column(primary_key=True)
+    landlord_id: Mapped[int] = mapped_column(ForeignKey("landlords.id"))
+    name: Mapped[str] = mapped_column()
+    cpf: Mapped[str] = mapped_column(String(11))
+    email: Mapped[str] = mapped_column()
+    phone: Mapped[str] = mapped_column()
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+
+    __table_args__ = (
+        UniqueConstraint("landlord_id", "cpf", name="uq_client_landlord_cpf"),
+    )
