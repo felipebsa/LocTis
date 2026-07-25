df726953966b343e443986ba3f5493c7f1565f0c
felipessantosbsa-cypher <felipessantosbsa@gmail.com>
2026-07-25 19:05:40 -0300

    feat: add get_current_user --> routes security

--- backend/core/security.py
+++ backend/core/security.py
@@ -1,8 +1,15 @@
 from datetime import datetime, timedelta, timezone
-from jose import jwt
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+from fastapi import Depends
 import os
 from dotenv import load_dotenv
 from passlib.context import CryptContext
+from database import get_db
+from sqlalchemy.orm import Session
+from fastapi import HTTPException
+from models.landlord import Landlord
+from sqlalchemy import select
 
 load_dotenv()
 
@@ -12,6 +19,7 @@ ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("TOKEN_EXPIRED"))
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 def create_access_token(landlord_id: int) -> str:
     expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
@@ -24,3 +32,18 @@ def hash_password(password: str):
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Token invalidated")
+
+    landlord_id = payload.get("sub")
+    user_query = select(Landlord).where(Landlord.id==landlord_id)
+    landlord = db.execute(user_query).scalars().first()
+
+    if not landlord:
+        raise HTTPException(status_code=401, detail="Token inválido")
+
+    return landlord
