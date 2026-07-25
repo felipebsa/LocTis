afdadccee7a8124fa69654f27597834fbfca243a
felipessantosbsa-cypher <felipessantosbsa@gmail.com>
2026-07-25 18:45:02 -0300

    feat: add security.py --> configs jwt

---
+++ backend/core/security.py
@@ -0,0 +1,26 @@
+from datetime import datetime, timedelta
+from jose import jwt
+import os
+from dotenv import load_dotenv
+from passlib.context import CryptContext
+
+load_dotenv()
+
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("TOKEN_EXPIRED"))
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def create_access_token(landlord_id: int) -> str:
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    payload = {"sub": str(landlord_id), "exp": expire}
+    token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+    return token
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
