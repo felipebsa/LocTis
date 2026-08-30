ecad1f7334b60be277de99998717ec1a3fe41858
felipebsa <felipessantosbsa@gmail.com>
2026-08-30 16:50:37 -0300

    feat: login test

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -10,3 +10,9 @@ def test_register():
     response = client.post("/auth/register", json=payload)
     
     assert response.status_code == 201
+
+def test_login():
+    client.post("/auth/register", json={"name": "roberto", "email": "roberto123@gmail.com", "password": "123456"})
+    response = client.post("/auth/login", data={"username": "roberto123@gmail.com", "password": "123456"})
+    assert response.status_code == 200
+    assert "access_token" in response.json()
